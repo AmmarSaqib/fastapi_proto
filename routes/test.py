c17551237a38fb720012a838372f29e8c4dfc9d8
AmmarSaqib <ammarsaqib123@gmail.com>
2021-03-04 01:00:24 +0500

    my boiler plate

--- routes/test.py
+++ routes/test.py
@@ -1,40 +1,29 @@
 from typing import Optional
-from enum import Enum
+from fastapi import APIRouter
+from app.schemas import ModelName, Item
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+router = APIRouter(
+    prefix="/test",
+    tags=['test']
+)
 
-app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-    
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-@app.get("/")
+@router.get("/")
 def read_root():
     """
     Testing this shit
     """
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
+@router.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
+@router.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
 
-@app.get("/models/{model_name}")
+@router.get("/{model_name}")
 def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
@@ -43,7 +32,3 @@ def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/files//home/heello/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
