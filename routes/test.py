1a5bd98895566a6b7584f760c673640d3316ab9d
AmmarSaqib <ammarsaqib123@gmail.com>
2021-03-09 11:34:13 +0500

    testing this repo

--- routes/test.py
+++ routes/test.py
@@ -1,3 +1,8 @@
+"""
+main routes file
+"""
+
+
 from typing import Optional
 from fastapi import APIRouter
 from app.schemas import ModelName, Item
@@ -15,16 +20,25 @@ def read_root():
     return {"Hello": "World"}
 
 @router.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, _q: Optional[str] = None):
+    """
+    Read Item
+    """
+    return {"item_id": item_id, "q": _q}
 
 
 @router.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
+    """
+    Update Item
+    """
     return {"item_price": item.price, "item_id": item_id}
 
 @router.get("/{model_name}")
 def get_model(model_name: ModelName):
+    """
+    Get model
+    """
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
